e0d17daa917644730077e43d5016e4fe2e59188a
Konstantin Kazmiruk <konstantinkazmiruk@gmail.com>
2022-07-07 22:06:06 +0300

    Creating the Mobile First Design for our plans

--- shared.js
+++ shared.js
@@ -7,27 +7,40 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 //console.dir(backdrop);
 //console.dir(selectPlanButton);
+//console.dir(backdrop.style['background-image']);
+
 for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener("click", function () {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    //modal.style.display = "block";
+    //backdrop.style.display = "block";
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
+  //mobileNav.style.display = "none";
+  mobileNav.classList.remove('open');
   closeModal();
 });
 
-backdrop.addEventListener("click", closeModal);
-modalNoButton.addEventListener("click", closeModal);
+if(modalNoButton){
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  //backdrop.style.display = "none";
+  //modal.style.display = "none";
+  if(modal){
+    modal.classList.remove('open');
+  }
+  backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+    console.log('toggle down');
+  //mobileNav.style.display = "block";
+  //backdrop.style.display = "block";
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
