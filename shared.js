f7c5916dfadbb66a4faea4b58df5ad0aac841852
Konstantin Kazmiruk <konstantinkazmiruk@gmail.com>
2022-07-07 14:56:18 +0300

    opening and closing the Hamburger Menu

---
+++ shared.js
@@ -0,0 +1,33 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButton = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+//console.dir(backdrop);
+//console.dir(selectPlanButton);
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
+
+backdrop.addEventListener("click", closeModal);
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
