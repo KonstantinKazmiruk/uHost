59dcfe4b40a7637e551b52a06d325bda901cf235
Konstantin Kazmiruk <konstantinkazmiruk@gmail.com>
2022-07-13 18:01:56 +0300

    added transitions and animations

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButton = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 //console.dir(backdrop);
 //console.dir(selectPlanButton);
@@ -14,7 +15,12 @@ for (var i = 0; i < selectPlanButton.length; i++) {
     //modal.style.display = "block";
     //backdrop.style.display = "block";
     modal.classList.add('open');
+    backdrop.style.display = 'block';
     backdrop.classList.add('open');
+    setTimeout(function() {
+      backdrop.classList.add('open');
+    }, 200)
+    
   });
 }
 
@@ -28,6 +34,17 @@ if(modalNoButton){
     modalNoButton.addEventListener("click", closeModal);
 }
 
+toggleButton.addEventListener("click", function () {
+  //mobileNav.style.display = "block";
+  //backdrop.style.display = "block";
+  mobileNav.classList.add('open');
+  backdrop.style.display = 'block';
+  setTimeout(function() {
+    backdrop.classList.add('open');
+  }, 200);
+});
+
+
 function closeModal() {
   //backdrop.style.display = "none";
   //modal.style.display = "none";
@@ -35,12 +52,17 @@ function closeModal() {
     modal.classList.remove('open');
   }
   backdrop.classList.remove('open');
+  setTimeout(function() {
+    backdrop.style.display = 'none';
+  }, 200);
 }
 
-toggleButton.addEventListener("click", function () {
-    console.log('toggle down');
-  //mobileNav.style.display = "block";
-  //backdrop.style.display = "block";
-  mobileNav.classList.add('open');
-  backdrop.classList.add('open');
+ctaButton.addEventListener('animationstart', function(event){
+  console.log('animation started', event);
+});
+ctaButton.addEventListener('animationitaration', function(event){
+  console.log('animation itaration', event);
+});
+ctaButton.addEventListener('animationend', function(event){
+  console.log('animation ended', event);
 });
